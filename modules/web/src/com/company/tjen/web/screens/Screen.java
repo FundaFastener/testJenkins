499355a54be1cf64fe5bdbe8edc9f2a732372d25
Howard Funda <howard@funda-fastener.com>
2018-04-03 16:25:22 +0800

    cuba in

---
+++ modules/web/src/com/company/tjen/web/screens/Screen.java
@@ -0,0 +1,9 @@
+package com.company.tjen.web.screens;
+
+import com.haulmont.cuba.gui.components.AbstractWindow;
+
+public class Screen extends AbstractWindow {
+
+    public void onClickMe() {
+    }
+}
